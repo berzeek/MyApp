6acf9858014652985e907c9ea18e34b41310e7e2
brandon <zkbrndn@yahoo.com>
2017-01-18 13:36:20 -0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dash', function () {
+    return view('dash');
+});
+
+Route::get('/charts', function () {
+    return view('charts');
+});
+
+Route::get('/forms', function () {
+    return view('forms');
+});
+
+Route::get('/icons', function () {
+    return view('icons');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/panels', function () {
+    return view('panels');
+});
+
+Route::get('/tables', function () {
+    return view('tables');
+});
+
+Route::get('/widgets', function () {
+    return view('widgets');
+});
